79fb283de6ce64ce96ae4c9316e61641386eaf5a
Matthew Rourke <mrourke@gmail.com>
2015-04-08 17:46:43 -0400

    floobits

---
+++ client/models/SongModel.js
@@ -0,0 +1,13 @@
+// SongModel.js - Defines a backbone model class for songs.
+var SongModel = Backbone.Model.extend({
+
+  play: function(){
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('play', this);
+  },
+  
+  enqueue: function(){
+
+  }
+
+});
